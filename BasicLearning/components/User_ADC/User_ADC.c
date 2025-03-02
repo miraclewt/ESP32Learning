d5a1801d3052c3fcd819c61a53e566417149018d
miraclewt <1830400832@qq.com>
2025-03-02 21:42:30 +0800

    Add Adc 模块

---
+++ BasicLearning/components/User_ADC/User_ADC.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "User_ADC.h"
+
+void User_ADC_Init(void)
+{
+    adc_oneshot_unit_handle_t adc1_handle;
+}
---
+++ BasicLearning/components/User_ADC/include/User_ADC.h
@@ -0,0 +1,3 @@
+#include "esp_adc/adc_oneshot.h"
+
+void User_ADC_Init(void);
